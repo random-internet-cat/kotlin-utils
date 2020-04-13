77cab701a44abd7c765ea4cf5015158f486ea32d
Jason Cobb <jason.e.cobb@gmail.com>
2020-04-12 21:33:16 -0400

    Ensure that consistent Kotlin version is always used

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     kotlin("jvm") version "1.3.71"
 }
 
+// Must always be updated along with plugins block
+val kotlinVersion = "1.3.71"
+
 group = "io.github.random-internet-cat"
 version = "1.0.0-SNAPSHOT"
 
@@ -10,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib-jdk8", kotlinVersion))
 }
 
 tasks {
