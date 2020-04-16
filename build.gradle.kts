4b08f9a74b92c300fc66e99ea97e96d317a50433
Jason Cobb <jason.e.cobb@gmail.com>
2020-04-15 23:30:16 -0400

    Add publishing information to build.gradle.kts (#10)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.3.71"
+    id("maven-publish")
+    id("com.jfrog.bintray") version "1.8.5"
 }
 
 // Must always be updated along with plugins block
@@ -7,6 +9,53 @@ val kotlinVersion = "1.3.71"
 
 group = "io.github.random-internet-cat"
 version = "1.0.0"
+
+val bintrayRepo = "kotlin-utils"
+
+val artifactName = project.name
+val artifactGroup = project.group.toString()
+val artifactVersion = project.version.toString()
+
+val githubUrl = "https://github.com/random-internet-cat/kotlin-utils"
+val licenseName = "MIT"
+
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.getByName("main").allSource)
+}
+
+val mavenPublicationName = "maven-publication"
+
+publishing {
+    publications {
+        create<MavenPublication>(mavenPublicationName) {
+            groupId = artifactGroup
+            artifactId = artifactName
+            version = artifactVersion
+            from(components["kotlin"])
+
+            artifact(sourcesJar)
+        }
+    }
+}
+
+bintray {
+    user = project.findProperty("bintrayUser").toString()
+    key = project.findProperty("bintrayKey").toString()
+
+    pkg.apply {
+        repo = bintrayRepo
+        name = artifactName
+        vcsUrl = githubUrl
+
+        setLicenses(licenseName)
+        setPublications(mavenPublicationName)
+
+        version.apply {
+            name = artifactVersion
+        }
+    }
+}
 
 repositories {
     mavenCentral()
